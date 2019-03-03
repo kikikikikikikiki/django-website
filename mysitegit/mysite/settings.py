cd9992840595e7a66e27a957ac805e6033970bc3
kikikikikikikiki <29565981+kikikikikikikiki@users.noreply.github.com>
2019-03-03 02:41:44 +0000

    Updated image links
    Changed image links to point to static files on server rather than outsde sources.

--- mysitegit/mysite/settings.py
+++ mysitegit/mysite/settings.py
@@ -121,20 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = '/var/www/mysite/static/'
+STATIC_ROOT = '/var/www/mysite/static/'
 
 
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
-
-#ENV_PATH = os.path.abspath(os.path.dirname(__file__))
-#MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
-
-
 
 
 TINYMCE_DEFAULT_CONFIG = {
